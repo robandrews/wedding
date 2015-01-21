cfef474d4cd340e86398bed4aaec8ca6fa1fe0ce
Rob Andrews <rob.andrews@alumni.stanford.edu>
2015-01-20 22:49:40 -0800

    first commit, positioned picture

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wedding::Application.config.secret_key_base = '38f087a59653608e85576ad699505ac9a4b8c0cf0452929651f791e3715eac1d76aa7be920af2e35b5f740db2a070eb2647507bbe884b00015d32f6c27dc2156'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Wedding::Application.routes.draw do
+  root to: 'static_pages#home'
+end
