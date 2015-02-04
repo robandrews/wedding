bd4349240ff7a92e0da686c715665432bc777343
Rob Andrews <rob.andrews@alumni.stanford.edu>
2015-02-03 22:31:57 -0800

    animate css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,12 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+var ready = function(){
+	$('.names').removeClass("hidden");
+	$('.names').addClass('animated fadeInDown');
+	$('.date').removeClass("hidden");
+	$('.date').addClass('animated fadeInDown');
+}
+
+setTimeout(ready, 1500);
