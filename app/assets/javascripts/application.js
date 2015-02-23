e9fb6d097f4a529ae0495401f76564906846b51c
Rob Andrews <rob.andrews@alumni.stanford.edu>
2015-02-22 22:44:53 -0800

    google maps for real, link styling, smooth scroll

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,6 @@
 //= require jquery
 //= require jquery_ujs
+//= require animatescroll.min.js
 //= require_tree .
 
 // var ready = function(){
@@ -19,3 +20,64 @@
 // setTimeout(ready, 1200);
 
 
+var ready = function(){
+  $(".home-link").click(function(event){
+    event.preventDefault();
+    var name = $(event.target).attr("data-name");
+    $("#"+name).animatescroll({
+      easing: "swing"
+    })
+  })
+}
+
+$(ready)
+
+
+
+// The following example creates a marker in Stockholm, Sweden
+// // using a DROP animation. Clicking on the marker will toggle
+// // the animation between a BOUNCE animation and no animation.
+//38.452427,-122.703247
+var church = new google.maps.LatLng(38.452427, -122.703247);
+var reception = new google.maps.LatLng(38.6204845, -122.864779);
+var center = new google.maps.LatLng(38.543795, -122.772683);
+var marker;
+var map;
+
+function initialize() {
+  var mapOptions = {
+    zoom: 10,
+    center: center,
+    scrollwheel: false
+  };
+
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+                            mapOptions);
+
+                            churchMarker = new google.maps.Marker({
+                              map:map,
+                              draggable:false,
+                              animation: google.maps.Animation.DROP,
+                              position: church
+                            });
+
+                            receptionMarker = new google.maps.Marker({
+                              map:map,
+                              draggable:false,
+                              animation: google.maps.Animation.DROP,
+                              position: reception
+                            });
+
+                            google.maps.event.addListener(churchMarker, 'click', toggleBounce);
+}
+
+function toggleBounce() {
+
+  if (marker.getAnimation() != null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
+
+google.maps.event.addDomListener(window, 'load', function(){setTimeout(initialize, 1000)});
